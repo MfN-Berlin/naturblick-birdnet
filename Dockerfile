a896506a9a5e5bbdc7e961dc406636dd93f583cc
Petter Arvidsson <petter.arvidsson@mfn.berlin>
2025-12-02 11:42:16 +0100

    Add warmup for librosa
    - Track time consumption for download, decode and inference

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM python:3.13
 
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
-
 WORKDIR /app
 
 ADD https://zenodo.org/records/17631020/files/BirdNET+_V3.0-preview2_EUNA_1K_FP32.pt?download=1 /app/
 
 ADD https://zenodo.org/records/17631020/files/BirdNET+_V3.0-preview2_EUNA_1K_Labels.csv?download=1 /app/
+
+ADD https://naturblick.museumfuernaturkunde.berlin/media/audio_files/amphibian_d52fd27a_06b4d9c428.mp3 /app/
 
 COPY requirements.txt /app/
 
@@ -15,6 +14,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
 
-CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8080", "--preload", "app:app"]
+CMD ["gunicorn", "-w", "3", "-b", "0.0.0.0:8080", "--preload", "app:app"]
 
 EXPOSE 8080
--- app.py
+++ app.py
@@ -6,12 +6,19 @@ from urllib import parse
 import csv
 import json
 import numpy as np
+import time
+import logging
+
+logger = logging.getLogger("gunicorn.error")
 
 SR = 32000
 
+# Create device and load model
 device = torch.device("cpu")
 model = torch.jit.load("BirdNET+_V3.0-preview2_EUNA_1K_FP32.pt", map_location=device)
 model.eval()
+
+# Load labels
 labels = []
 with open("BirdNET+_V3.0-preview2_EUNA_1K_Labels.csv", "r", newline="", encoding="utf-8") as f:
     reader = csv.reader(f, delimiter=";")
@@ -19,9 +26,20 @@ with open("BirdNET+_V3.0-preview2_EUNA_1K_Labels.csv", "r", newline="", encoding
     for row in reader:
         labels.append(int(row[0].strip()))
 
-def run_inference(url, start, end):
-    y, _ = librosa.load(io.BytesIO(request.urlopen(url).read()), sr=SR, mono=True, offset=float(start) / 1000.0, duration = float(end - start) / 1000.0)
-    chunks = np.stack([y], axis=0)
+# Warm up librosa, initial call can be _very_ slow
+warmup_time = time.perf_counter()
+librosa.load("amphibian_d52fd27a_06b4d9c428.mp3")
+logger.info(f"Finished warm-up: {time.perf_counter() - warmup_time}")
+
+def download_audio(url):
+    return io.BytesIO(request.urlopen(url).read())
+
+def decode_audio(encoded, start, end):
+    return librosa.load(encoded, sr=SR, mono=True, offset=float(start) / 1000.0, duration = float(end - start) / 1000.0)[0]
+
+
+def run_inference(audio):
+    chunks = np.stack([audio], axis=0)
     with torch.inference_mode():
         x = torch.from_numpy(chunks).to(device)
         return model(x)[1][0].detach().cpu().numpy().astype(float)
@@ -32,7 +50,14 @@ def app(environ, start_response):
     start = int(params["start"])
     end = int(params["end"])
     url = params["url"]
-    result = run_inference(url, start, end)
+    download_time = time.perf_counter()
+    encoded = download_audio(url)
+    decode_time = time.perf_counter()
+    decoded = decode_audio(encoded, start, end)
+    model_time = time.perf_counter()
+    result = run_inference(decoded)
+    finish_time = time.perf_counter()
+    logger.info(f"URL: {url} start: {start} end: {end} download: {decode_time - download_time:.3f}s decode: {model_time - decode_time:.3f}s model: {finish_time - model_time:.3f}s")
     results_with_labels = sorted(zip(labels, result), key = lambda r: r[1], reverse = True)
     data = json.dumps([{"id": id, "score": e} for id, e in results_with_labels[:3]]).encode("UTF-8")
     response_headers = [
